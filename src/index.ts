786a1399e780c8c75f3f79b9d4da8a20f4558a18
Josetic224 <josephochiagha112@gmail.com>
2024-02-25 14:14:48 +0100

    added / route

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(helmet());
 const port: number = 3000;
 
 // Define a route for the homepage
-app.get("/api_v1/homepage", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("<h1>Welcome to Plaintiff Aid Backend!</h1>");
 });
 
