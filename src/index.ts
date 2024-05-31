5781fc28a71595987be2ed07f1ae28c3000d0fd7
Joseph Ochiagha <ochiaghaifeanyichukwu185@gmail.com>
2024-05-31 11:59:21 +0100

    cors policy fixed version 2

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,31 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import routers from "./routers";
 import helmet from "helmet";
-import cors from "cors"
+import cors, {CorsOptions} from "cors"
+import bodyParser from "body-parser";
 import { connectToDatabase, prisma } from "./db/users.db";
 const app: Application = express();
+const allowedOrigins = ['http://localhost:3000', 'https://plaintiffaid.vercel.app'];
 
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
+// Middleware
+app.use(cors(corsOptions));
+app.use(bodyParser.json()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 async function startApp() {
   try {
@@ -23,12 +43,9 @@ async function startApp() {
 }
 
 startApp();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
 
 
-app.use(cors);
+
 
 
 
